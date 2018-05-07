567a60e55968ea29eee699a27938799784691a73
Kiana Caston <castonkr@rose-hulman.edu>
2018-05-07 12:05:57 -0400

    We got an earth!

--- Meteor Shower/ViewController.swift
+++ Meteor Shower/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    let earthRadiusKm: CGFloat = 6371
+    let moonRadiusKm: CGFloat = 1731.5
+    let earthMoonDistanceKm: CGFloat = 384400 / 10 // Divide actual distance by 10 to cheat and make the moon closer
+    let scale: CGFloat = 1 / 50000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
+                                 ARSCNDebugOptions.showFeaturePoints]
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.session.run(configuration)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pressedAddEarth(_ sender: Any) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: earthRadiusKm * scale)
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "earth_daymap")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "earth_specular_map")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "earth_clouds")
+        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "earth_elevation_normal_map")
+        
+        earth.position = SCNVector3(0, 0, -0.5)
+        
+        sceneView.scene.rootNode.addChildNode(earth)
+        
+        let earthRotationAction = SCNAction.rotateBy(x: 0,
+                                                     y: CGFloat(360.degreesToRadians),
+                                                     z: 0,
+                                                     duration: 8.0)
+    
+        let earthRotateForever = SCNAction.repeatForever(earthRotationAction)
+        earth.runAction(earthRotateForever)
     }
-
-
+    
+    @IBAction func pressedFireMeteor(_ sender: Any) {
+        
+    }
+    
 }
 
+extension Int {
+    var degreesToRadians: Double {
+        return Double(self) * .pi / 180.0
+    }
+}
